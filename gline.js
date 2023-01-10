e9cb476450be875ca8a59b958d03d77cb2cb08d0
Karma The Analyst <karmagrg23@gmail.com>
2023-01-10 11:57:04 -0500

    first commit

---
+++ gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Year','umich.edu','ufp.pt','ucdavis.edu','indiana.edu','berkeley.edu','uct.ac.za','columbia.edu','mac.com','mtu.edu','gmail.com','unl.edu','virginia.edu','weber.edu','etudes.org','cam.ac.uk'],
+['2005-12',57,10,11,12,12,14,13,12,11,10,12,9,8,5,7],
+['2006-01',18,13,11,7,5,3,3,3,1,2,0,1,2,5,2]
+];
